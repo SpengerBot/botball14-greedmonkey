4b853eae71e85fd0c0771986d9b9b756aba2b1ae
Yanik Thurner <thurner@yanik.at>
2014-03-27 18:55:16 +0100

    Second Try to catch the cube, startup with scorpion perfect

--- GreedMonkey Test/GreedMonkeyTest.c
+++ GreedMonkey Test/GreedMonkeyTest.c
@@ -1,13 +1,77 @@
-void main2()
+void turn(int times); 
+//Motors
+#define Motor_Left 2
+#define Motor_Right 0
+#define Motor_Up 1
+
+//Servos
+#define	Servo_Left 3
+#define Servo_Right 1
+#define Servo_Back 0
+
+#define Servo_Back_Up 2000
+#define Servo_Back_Down 0
+#define Servo_Left_Open 1250
+#define Servo_Left_Closed 375
+#define Servo_Right_Open 1000
+#define Servo_Right_Closed 2030
+
+//Button
+#define Button_Up 14
+
+//Sensor Wert
+#define Sensor_Up 1
+#define Sensor_Up_Value 600
+
+//Camera Channel
+#define Yellow_Channel 0
+
+//Motor Werte
+#define Motor_up_speed 100
+#define Motor_down_speed -30
+
+//turn
+//#define turn_x 0
+
+#define wert 575
+void main()
 {
-	camera_open_at_res(0);
-	display_clear();
-	camera_update();
-	while(1){
-		display_clear();
-		camera_update();
-		//printf("get_object_center(0,0).x: %d\n",get_object_center(0,0).x);
-		printf("get_object_bbox(0,0).x: %d\n",get_object_bbox(0,0).width);
-		msleep(200);
+	
+	turn(4);
+}
+
+void turn(int times)
+{
+	int turn_x = 0;
+	
+	if(times > 0)
+	{
+		while(turn_x < times)
+		{	
+			motor(Motor_Left,100);
+			motor(Motor_Right,-100);
+			msleep(680);
+			freeze(Motor_Left);
+			freeze(Motor_Right);
+			printf("%d",turn_x*90);
+			turn_x++;
+			msleep(1000);
+		}
 	}
+	else
+	{
+		while(turn_x < times*-1)
+		{	
+			motor(Motor_Left,100*-1);
+			motor(Motor_Right,-100*-1);
+			msleep(680);
+			freeze(Motor_Left);
+			freeze(Motor_Right);
+			printf("%d",turn_x*90);
+			turn_x++;
+			msleep(1000);
+		}
+	}
+	
+	
 }
--- GreedMonkey/GreedMonkey.c
+++ GreedMonkey/GreedMonkey.c
@@ -4,17 +4,21 @@ void go_for_cube();
 void camera_fix();
 void calibrate();
 void cube_is_near();
-
+void take_position();
+void claw_up();
+void claw_down();
+void claw_close();
+void claw_open();
 // Created on Mo März 24 2014
 //Motors
-#define Motor_Left 2
-#define Motor_Right 0
-#define Motor_Up 1
+#define Motor_Left 0
+#define Motor_Right 2
+#define Motor_Up 3
 
 //Servos
-#define	Servo_Left 3
+#define	Servo_Left 0
 #define Servo_Right 1
-#define Servo_Back 0
+#define Servo_Back 3
 
 #define Servo_Back_Up 2000
 #define Servo_Back_Down 0
@@ -26,17 +30,24 @@ void cube_is_near();
 //Button
 #define Button_Up 14
 
+//Sensor + Wert
+#define Sensor_Up 1
+#define Sensor_Up_Value 600
+
 //Camera Channel
 #define Yellow_Channel 0
 
 //Motor Werte
-#define up_Motor_up_speed 100
-#define up_Motor_down_speed -30
+#define Motor_up_speed 100
+#define Motor_down_speed -25
+#define Motor_fast_down_speed -100
 
 //Klauen Werte
 int klaue_down = 0;
 int klaue_orange=0;
-int klaue_yellow=0;
+
+//orange only 
+//int klaue_yellow=0;
 int klaue_start=0;
 
 //start 48500
@@ -44,11 +55,23 @@ int klaue_start=0;
 void main()
 {
 	calibrate();
-	enable_servos();
 	start_position();
-	watch_for_cube();
+	take_position();
+	msleep(2000);
+	camera_open();
 	go_for_cube();
-	cube_is_near();
+	//cube_is_near();
+	
+	
+	//motor(Motor_Up,Motor_fast_down_speed);
+	//while(!digital(Button_Up)){}
+	//ao();
+	//wait so it doesn't fuck up
+	//msleep(500);
+	//up for straffes seil
+	//motor(Motor_Up,Motor_up_speed);
+	//while(digital(Button_Up)){}
+	//ao();
 }
 
 void calibrate(){
@@ -59,42 +82,89 @@ void calibrate(){
 	while(!b_button()){}
 	display_clear();
 	printf("calibrating");
-	motor(Motor_Up,up_Motor_down_speed);
+	
+	motor(Motor_Up,Motor_fast_down_speed);
 	while(!digital(Button_Up)){}
 	ao();
-	motor(Motor_Up,up_Motor_up_speed);
+	//wait so it doesn't fuck up
+	msleep(500);
+	//up for straffes seil
+	motor(Motor_Up,Motor_up_speed);
 	while(digital(Button_Up)){}
 	ao();
-	clear_motor_position_counter(Motor_Up);
-	klaue_down=get_motor_position_counter(Motor_Up);
-	printf("Motor Position: %d", klaue_down);
-	move_to_position(Motor_Up,100,10000);
 	
+	enable_servos();
+	claw_close();
+	
+	set_servo_position(Servo_Back, Servo_Back_Up);	
 }
 
 void start_position(){
-	set_servo_position(Servo_Back, Servo_Back_Up);
+	printf("wait for light oida");
+	set_b_button_text("I am the Twilight");
+	while(!b_button()){}
+	msleep(1000);
+	
+	motor(Motor_Up,Motor_up_speed);
+	motor(Motor_Left,-100);
+	motor(Motor_Right,100);
+	msleep(800);
+	freeze(Motor_Left);
+	freeze(Motor_Right);
+	msleep(1200);
 	motor(Motor_Left,100);
-	motor(Motor_Right,25);
-	msleep(2500);
-	ao();
-	set_servo_position(Servo_Left, Servo_Left_Open);
-	set_servo_position(Servo_Right, Servo_Right_Open);
+	motor(Motor_Right,100);
+	msleep(1200);
+	freeze(Motor_Left);
+	freeze(Motor_Right);
+	//set_servo_position(Servo_Left, Servo_Left_Open);
+	//set_servo_position(Servo_Right, Servo_Right_Open);
+}
+void take_position()
+{	
+	//zurück 2sec
+	motor(Motor_Left,-100);
+	motor(Motor_Right,-100);
+	msleep(1700);
+	freeze(Motor_Left);
+	freeze(Motor_Right);
+
+	//90°
+	motor(Motor_Left,100);
+	motor(Motor_Right,-100);
+	msleep(800);
+	freeze(Motor_Left);
+	freeze(Motor_Right);
+	msleep(2000);
+
+	motor(Motor_Left,100);
+	motor(Motor_Right,100);
+	msleep(1500);
+	
+	motor(Motor_Left,100);
+	motor(Motor_Right,-100);
+	msleep(750);
+	
+	freeze(Motor_Left);
+	freeze(Motor_Right);
+	while(analog(Sensor_Up) > Sensor_Up_Value){}
+	freeze(Motor_Up);
 }
 void go_for_cube()
 {
 	camera_fix();
 	printf("echo: %d\n",get_object_bbox(0,0).width);
-	while(get_object_bbox(0,0).width < 50){
+	claw_open();
+	while(get_object_bbox(0,0).width < 40){
 		camera_update();
 		printf("search: %d\n",get_object_center(0,0).x);
-		if(get_object_center(0,0).x > 100)
+		if(get_object_center(0,0).x > 110)
 		{
 			printf("it's right\n");
 			motor(Motor_Left,5);
 			motor(Motor_Right,-5);
 		}
-		else if(get_object_center(0,0).x < 60)
+		else if(get_object_center(0,0).x < 50)
 		{
 			printf("it's left\n");
 			motor(Motor_Left,-5);
@@ -107,31 +177,35 @@ void go_for_cube()
 			motor(Motor_Right,5);
 		}
 	}
+	motor(Motor_Left,100);
+	motor(Motor_Right,100);
+	msleep(500);
+	claw_close();
 }
-void watch_for_cube(){
-	camera_open_at_res(0);
-	display_clear();
-	camera_update();
-	while(get_object_count(0)<1){
-		camera_update();
-	}
-	while(get_object_center(0,0).x > 100 || get_object_center(0,0).x < 60){
-		camera_update();
-		printf("Column: %d\n",get_object_center(0,0).x);
-		if(get_object_center(0,0).x > 100)
-		{
-			motor(Motor_Left,5);
-			motor(Motor_Right,-5);
-		}
-		if(get_object_center(0,0).x < 60)
-		{
-			motor(Motor_Left,-5);
-			motor(Motor_Right,5);
-		}
-	}
-	printf("Last Col: %d\n",get_object_center(0,0).x);
+void claw_close(){
+	set_servo_position(Servo_Left,Servo_Left_Closed);
+	set_servo_position(Servo_Right,Servo_Right_Closed);
+}
+void claw_open(){
+	set_servo_position(Servo_Left,Servo_Left_Open);
+	set_servo_position(Servo_Right,Servo_Right_Open);
+}
+void claw_up(){
+	motor(Motor_Up,Motor_up_speed);
+	while(analog(Sensor_Up) > Sensor_Up_Value){}
 	ao();
-	
+}
+
+void claw_down(){
+	motor(Motor_Up,Motor_down_speed);
+	while(!digital(Button_Up)){}
+	ao();
+	//wait so it doesn't fuck up
+	msleep(500);
+	//up for straffes seil
+	motor(Motor_Up,Motor_up_speed);
+	while(digital(Button_Up)){}
+	ao();
 }
 void cube_is_near(){
 	camera_fix();
@@ -139,9 +213,15 @@ void cube_is_near(){
 }
 
 void camera_fix(){
-	double seconds_now=seconds()+3;
-	while(seconds() < seconds_now){
-		camera_update();
-	}
+	printf("camerafix\n");
+	//double seconds_now=seconds()+3;
+	//while(seconds() < seconds_now){
+	//	camera_update();
+	//}
+	camera_update();
+	msleep(3000);
+	camera_update();
+	printf("camerafix done\n");
 }
+
 
